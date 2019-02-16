c6821eda4be0db867453d4e6ea9e2aa1499c000d
siddharh <siddharthjn93@gmail.com>
2019-02-16 17:30:12 +0530

    pages setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {ReactRouter, Router} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
---
+++ src/pages/ReadMe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ReadMe() {
+  return (
+    <div>
+      Read me
+    </div>
+  )
+}
